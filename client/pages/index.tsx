45ee55ab46c21d17a3d515dd5ce452dc501cfd11
jiminykbob <jikang92@hotmail.com>
2019-10-03 18:18:43 -0700

    fix stuff

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,30 +1,31 @@
 import React from 'react';
 import Head from 'next/head';
 import Layout from '../components/Layout';
-// import { useMeQuery } from '../generated/graphql';
+import { useMeQuery } from '../generated/graphql';
 
 interface Props {}
 
 const Home: React.FC<Props> = () => {
-  const { data } = useMeQuery({ fetchPolicy: "network-only" });
-  
+  const { data } = useMeQuery({ fetchPolicy: 'network-only' });
+
   if (!data) {
     return (
       <Layout>
         <div />
       </Layout>
-    )
-  };
+    );
+  }
 
   return (
     <Layout>
       <Head>
         <title>Cool App Title</title>
       </Head>
-    {
-      data && data.me ?
-      <div>You are logged in as {data.me.email}</div>
-        : <div>HOME</div>}
+      {data && data.me ? (
+        <div>You are logged in as {data.me.email}</div>
+      ) : (
+        <div>HOME</div>
+      )}
     </Layout>
   );
 };
