fb5ae333a58f4b71c5037a48e07a1c51a3f8d875
grantkang5 <g1k05j@gmail.com>
2019-10-04 16:05:37 -0700

    Removes react-router-dom and use next/router

--- client/pages/Bye.tsx
+++ client/pages/Bye.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { useByeQuery } from "../generated/graphql";
+import Layout from "../components/Layout";
 
 interface Props {}
 
 const Bye: React.FC<Props> = () => {
   const { data, error } = useByeQuery({ fetchPolicy: "network-only" });
   if (error) {
-    return <div>{error.message}</div>;
+    return <Layout>{error.message}</Layout>;
   }
-  return <div>{JSON.stringify(data)}</div>;
+  return <Layout>{JSON.stringify(data)}</Layout>;
 };
 
 export default Bye;
--- client/pages/Register.tsx
+++ client/pages/Register.tsx
@@ -1,45 +1,48 @@
 import React, { useState } from "react";
 import { useRegisterMutation } from "../generated/graphql";
-import { RouteComponentProps } from "react-router-dom";
+import Router from "next/router";
+import Layout from "../components/Layout";
 
-const Register: React.FC<RouteComponentProps> = ({ history }) => {
+const Register: React.FC = () => {
   const [email, handleEmail] = useState("");
   const [password, handlePassword] = useState("");
   const [register] = useRegisterMutation();
 
   return (
-    <form
-      onSubmit={async e => {
-        e.preventDefault();
-        console.log(email, password);
-        try {
-          await register({
-            variables: {
-              email,
-              password
-            }
-          });
-          history.push('/')
-        } catch (err) {
-          console.log(err)
-        }
-      }}
-    >
-      <input
-        value={email}
-        placeholder="test@email.com"
-        autoComplete='off'
-        onChange={e => handleEmail(e.target.value)}
-      />
-      <input
-        value={password}
-        autoComplete='off'
-        type="password"
-        placeholder="password"
-        onChange={e => handlePassword(e.target.value)}
-      />
-      <button type="submit">register</button>
-    </form>
+    <Layout>
+      <form
+        onSubmit={async e => {
+          e.preventDefault();
+          console.log(email, password);
+          try {
+            await register({
+              variables: {
+                email,
+                password
+              }
+            });
+            Router.push("/");
+          } catch (err) {
+            console.log(err);
+          }
+        }}
+      >
+        <input
+          value={email}
+          placeholder="test@email.com"
+          autoComplete="off"
+          onChange={e => handleEmail(e.target.value)}
+        />
+        <input
+          value={password}
+          autoComplete="off"
+          type="password"
+          placeholder="password"
+          onChange={e => handlePassword(e.target.value)}
+        />
+        <button type="submit">register</button>
+      </form>
+    </Layout>
   );
 };
 
