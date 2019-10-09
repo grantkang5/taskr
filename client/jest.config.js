f1d4afad53166f1bbd6f7450f8d199cd27b6f824
grantkang5 <g1k05j@gmail.com>
2019-10-08 19:26:51 -0700

    add enzyme+jest tests for pags

---
+++ client/jest.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  moduleFileExtensions: [
+    "ts",
+    "tsx",
+    "js"
+  ],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  // tslint:disable-next-line: object-literal-sort-keys
+  testMatch: [
+    "**/*.(test|spec).(ts|tsx)"
+  ],
+  globals: {
+    "ts-jest": {
+      useBabelrc: true,
+      // tslint:disable-next-line: object-literal-sort-keys
+      tsConfigFile: "jest.tsconfig.json"
+    }
+  },
+  coveragePathIgnorePatterns: [
+    "/node_modules/",
+    "enzyme.js"
+  ],
+  setupFilesAfterEnv: ["<rootDir>/enzyme.js"],
+  coverageReporters: [
+    "json",
+    "lcov",
+    "text",
+    "text-summary"
+  ],
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/mocks.js",
+    // tslint:disable-next-line: object-literal-sort-keys
+    "\\.(css|less|scss)$": "<rootDir>/__mocks__/mocks.js"
+  }
+};
