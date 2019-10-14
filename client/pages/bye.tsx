641a0e8b08869f51d46ad78756692e9b03dd8452
grantkang5 <g1k05j@gmail.com>
2019-10-14 13:06:57 -0700

    add next-css modules

--- client/pages/bye.tsx
+++ client/pages/bye.tsx
@@ -3,12 +3,12 @@ import { useByeQuery } from "../generated/graphql";
 import Layout from "../components/Layout";
 
 const Bye: React.FC = () => {
-  const { error } = useByeQuery({ fetchPolicy: "network-only" });
+  const { data, error } = useByeQuery({ fetchPolicy: "network-only" });
 
   if (error) {
     return <Layout>{error.message}</Layout>;
   }
-  return <Layout></Layout>;
+  return <Layout>{JSON.stringify(data)}</Layout>;
 };
 
 export default Bye;
--- client/pages/style.css.d.ts
+++
@@ -1,5 +0,0 @@
-declare const styles: {
-  readonly "something": string;
-};
-export = styles;
-
