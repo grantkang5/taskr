6e14524960415491a9da5e1b276615e2ac3c2c31
grantkang5 <g1k05j@gmail.com>
2019-10-14 14:39:39 -0700

    adds css/less modules

--- client/next.config.js
+++ client/next.config.js
@@ -1,14 +1,29 @@
-const withCSS = require('@zeit/next-css')
+const lessToJS = require("less-vars-to-js");
+const fs = require("fs");
+const path = require("path");
+const antdLessLoader = require("next-antd-aza-less")
 
-module.exports = withCSS({
-  cssLoaderOptions: {
-    importLoaders: 1,
-    localIdentName: "[local]___[hash:base64:5]",
-    url: false,
-  },
+const themeVariables = lessToJS(
+  fs.readFileSync(path.resolve(__dirname, './assets/antd-custom.less'), 'utf8')
+)
+
+if (typeof require !== 'undefined') {
+  // tslint:disable-next-line: no-empty
+  require.extensions['.less'] = file => {}
+}
+
+module.exports = antdLessLoader({
   cssModules: true,
+  lessLoaderOptions: {
+    importLoaders: 1,
+    javascriptEnabled: true,
+    localIdentName: "[local]___[hash:base64:5]",
+    modifyVars: themeVariables,
+    url: false
+  },
+  // tslint:disable-next-line: object-literal-sort-keys
   env: {
-    "API_URL": process.env.API_URL || "http://localhost:4000", 
-    "GRAPHQL_URL": process.env.GRAPHQL_URL || "http://localhost:4000/graphql"
+    API_URL: process.env.API_URL || "http://localhost:4000",
+    GRAPHQL_URL: process.env.GRAPHQL_URL || "http://localhost:4000/graphql"
   }
-})
+});
