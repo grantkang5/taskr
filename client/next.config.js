0aa59dbeb8db3fe4787bc3fc8cb8e5959fb7909b
grantkang5 <g1k05j@gmail.com>
2019-10-14 13:05:31 -0700

    asd

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,14 @@
-module.exports = {
+const withCSS = require('@zeit/next-css')
+
+module.exports = withCSS({
+  cssLoaderOptions: {
+    importLoaders: 1,
+    localIdentName: "[local]___[hash:base64:5]",
+    url: false,
+  },
+  cssModules: true,
   env: {
     "API_URL": process.env.API_URL || "http://localhost:4000", 
     "GRAPHQL_URL": process.env.GRAPHQL_URL || "http://localhost:4000/graphql"
   }
-}
+})
---
+++ client/pages/style.css.d.ts
@@ -0,0 +1,5 @@
+declare const styles: {
+  readonly "something": string;
+};
+export = styles;
+
