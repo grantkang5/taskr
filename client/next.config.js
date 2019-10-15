dab5d955c982d3123672980616fa8771702a0f7c
grantkang5 <g1k05j@gmail.com>
2019-10-15 15:05:20 -0700

    fix antd build error using styles

--- client/next.config.js
+++ client/next.config.js
@@ -25,5 +25,30 @@ module.exports = antdLessLoader({
   env: {
    "API_URL": process.env.API_URL || "http://localhost:4000",
     "GRAPHQL_URL": process.env.GRAPHQL_URL || "http://localhost:4000/graphql",
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      const antStyles = /antd\/.*?\/style.*?/
+      const origExternals = [...config.externals]
+      config.externals = [
+        (context, request, callback) => {
+          if (request.match(antStyles)) {
+            return callback()
+          }
+          if (typeof origExternals[0] === 'function') {
+            origExternals[0](context, request, callback)
+          } else {
+            callback()
+          }
+        },
+        ...(typeof origExternals[0] === 'function' ? [] : origExternals),
+      ]
+
+      config.module.rules.unshift({
+        test: antStyles,
+        use: 'null-loader',
+      })
+    }
+    return config
   }
 });
