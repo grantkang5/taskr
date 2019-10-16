30b524b649dd0ad5954daf16aa7406f858497eba
grantkang5 <g1k05j@gmail.com>
2019-10-15 18:58:56 -0700

    fix ssr render

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,12 +1,11 @@
 import React from "react";
-import Head from "next/head";
 import Layout from "../components/common/Layout";
 import { useMeQuery } from "../generated/graphql";
-
-import styles from "./Home.module.less";
 import { HeaderText, HeaderSubText } from "../components/common/Text";
 import { Button } from "antd";
 import Link from "next/link";
+
+import styles from "./Home.module.less";
 
 const Home: React.FC = () => {
   const { data } = useMeQuery();
@@ -25,13 +24,12 @@ const Home: React.FC = () => {
         <div className={styles.primaryHeader}>
           <div className={styles.left}>
             <HeaderText white={1} style={{ marginBottom: "28px" }}>
-              {"Plan and collaborate on projects faster than ever"}
+              Plan and collaborate on projects faster than ever
             </HeaderText>
 
             <HeaderSubText white={1} style={{ marginBottom: "28px" }}>
-              {
-                "Our app helps developers organize and plan out projects simply so you can prioritize development and pushing out features"
-              }
+              "Our app helps developers organize and plan out projects simply so
+              you can prioritize development and pushing out features"
             </HeaderSubText>
 
             <Link href="/register">
