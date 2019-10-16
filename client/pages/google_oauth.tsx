f820d4059dac1cf6fd97f16875c5ea2be4285cf0
jiminykbob <jikang92@hotmail.com>
2019-10-16 14:45:16 -0700

    lala

--- client/pages/google_oauth.tsx
+++ client/pages/google_oauth.tsx
@@ -3,32 +3,21 @@ import Router from 'next/router';
 import { setAccessToken, getAccessToken } from '../lib/accessToken';
 import { useAuth_GoogleOAuthMutation } from '../generated/graphql';
 
-interface Props {}
-
-const Google_OAuth: React.FC<Props> = () => {
+const GoogleOAuth: React.FC = () => {
   const [auth] = useAuth_GoogleOAuthMutation();
-  console.log('RENDERED GOOGLEOAUTH PAGE');
 
   useEffect(() => {
-    console.log('USEEFFECT TRIGGERED');
     let didCancel = false;
     const { code }: { code?: string } = Router.query;
-    console.log('code is', code);
     if (code) {
       const fetchGoogleUser = async () => {
-        console.log('code found. FETCHINGGOOGLEUSER');
         const response = await auth({
           variables: { code }
         });
 
-        console.log('response from server is', response);
         if (response && response.data) {
           setAccessToken(response.data.auth_googleOAuth.accessToken);
-          console.log('accessToken right after fetch is', getAccessToken());
           Router.push('/home');
-        }
-        if (!didCancel) {
-          console.log('response is', response);
         }
       };
       fetchGoogleUser();
@@ -41,4 +30,4 @@ const Google_OAuth: React.FC<Props> = () => {
   return <></>;
 };
 
-export default Google_OAuth;
+export default GoogleOAuth;
