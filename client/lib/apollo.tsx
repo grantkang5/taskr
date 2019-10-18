13014e42ff408c8a5138601aab1c22f61601e9da
grantkang5 <g1k05j@gmail.com>
2019-10-18 15:25:28 -0700

    jscookie

--- client/lib/apollo.tsx
+++ client/lib/apollo.tsx
@@ -11,6 +11,7 @@ import { getAccessToken, setAccessToken } from './accessToken';
 import { onError } from 'apollo-link-error';
 import { ApolloLink } from 'apollo-link';
 import cookie from 'cookie';
+import JSCookie from 'js-cookie'
 
 const isServer = () => typeof window === 'undefined';
 
@@ -63,7 +64,8 @@ export function withApollo(PageComponent: any, { ssr = true } = {}) {
       if (isServer()) {
         console.log('NEXT JS SERVER reading cookies from header: ', req.headers.cookie)
         console.log('fetching refresh token [nextjs] to ', process.env.API_URL!)
-
+        const randomCookie = JSCookie.get('qid')
+        console.log('JS COOKIE IS DA BEST: ', randomCookie)
         let cookies: any;
         if (req.headers.cookie) {
           cookies = cookie.parse(req.headers.cookie);
--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { ApolloProvider } from "@apollo/react-hooks";
 import { withApollo } from "../lib/apollo";
 import PrivateRoute from "../components/PrivateRoute";
+import Cookie from 'js-cookie'
 
 interface Props {
   apolloClient: any
@@ -11,6 +12,11 @@ interface Props {
 import './App.module.less'
 
 class MyApp extends App<Props> {
+  componentWillMount() {
+    const cookie = Cookie.get('qid')
+    console.log('THIS COOKIE IS DA BEST IN UNQWILLQMOUNT')
+  }
+
   render() {
     const { Component, pageProps, apolloClient } = this.props;
     return (
