02297f5ca60827bd3bc1b705dd243061e8cf2961
grantkang5 <g1k05j@gmail.com>
2019-10-18 11:46:20 -0700

    make cookies secure

--- server/src/services/auth/sendRefreshToken.ts
+++ server/src/services/auth/sendRefreshToken.ts
@@ -3,6 +3,7 @@ import { Response } from 'express';
 export const sendRefreshToken = (res: Response, token: string) => {
   res.cookie('qid', token, {
     httpOnly: true,
+    secure: process.env.NODE_ENV === 'production' ? true : false,
     // path: '/refresh_token',
     expires: new Date(Date.now() + 7 * 24 * 3600000)
   });
