593a8fc08b10b9487dfc1179d222c94bac6dce72
grantkang5 <g1k05j@gmail.com>
2019-10-18 15:18:19 -0700

    asd

--- server/src/services/auth/sendRefreshToken.ts
+++ server/src/services/auth/sendRefreshToken.ts
@@ -5,6 +5,7 @@ export const sendRefreshToken = (res: Response, token: string) => {
     httpOnly: true,
     /** disable path for next.js server */
     // path: "/refresh_token",
+    domain: '.dorya-client.com',
     expires: new Date(Date.now() + 7 * 24 * 3600000)
   });
 };
