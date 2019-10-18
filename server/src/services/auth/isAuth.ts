26a9a84f379a8a4e557a93bee28185cf3ab73cb6
grantkang5 <g1k05j@gmail.com>
2019-10-18 10:20:12 -0700

    add logs

--- server/src/services/auth/isAuth.ts
+++ server/src/services/auth/isAuth.ts
@@ -4,6 +4,7 @@ import { verify } from 'jsonwebtoken';
 
 export const isAuth: MiddlewareFn<MyContext> = ({ context }, next) => {
   const authorization = context.req.headers['authorization'];
+  console.log('Context req headers authorization: ', context.req.headers['authorization'])
   if (!authorization) {
     throw new Error('Not authenticated');
   }
