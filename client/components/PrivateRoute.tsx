14aa928d1d01200203e3b6bf880afc5a157035dc
grantkang5 <g1k05j@gmail.com>
2019-10-17 19:44:00 -0700

    readd privateoute

--- client/components/PrivateRoute.tsx
+++ client/components/PrivateRoute.tsx
@@ -1,32 +1,27 @@
-import React from 'react';
-import { useMeQuery } from '../generated/graphql';
-import { useRouter } from 'next/router';
-import { loadGetInitialProps } from 'next/dist/next-server/lib/utils';
+import React from "react";
+import { useMeQuery } from "../generated/graphql";
+import { useRouter } from "next/router";
 
 interface Props {
   children: React.ReactNode;
 }
 
-class PrivateRoute extends React.Component<Props> {
-  static async getInitialProps(ctx: any) {
-    console.log("PRIVATE ROUTE CONTEXT: ", ctx)
-  }
-  // const { data, loading } = useMeQuery();
+const PrivateRoute: React.FC<Props> = ({ children }) => {
+  const router = useRouter();
+  const { data, loading } = useMeQuery();
 
-  // const whitelist = ['/login', '/register', '/home', '/google'];
+  const whitelist = ["/login", "/register", "/home", "/google"];
 
-  // if (loading) {
-  //   // TODO: add load screen
-  //   return <></>;
-  // }
-  // // not authenticated, redirect unless it's in whitelist
-  // if (!whitelist.includes(router.route) && !data) {
-  //   router.push('/login');
-  //   return <></>;
-  // }
-  render() {
-    return <>{this.props.children}</>;
+  if (loading) {
+    // TODO: add load screen
+    return <></>;
   }
+  // not authenticated, redirect unless it's in whitelist
+  if (!whitelist.includes(router.route) && !data) {
+    router.push("/login");
+    return <></>;
+  }
+  return <>{children}</>;
 };
 
 export default PrivateRoute;
