ffc0f690e0fb489c9246d15d12c6661911b0a1b6
grantkang5 <g1k05j@gmail.com>
2019-10-18 16:01:31 -0700

    change domain

--- server/src/services/auth/sendRefreshToken.ts
+++ server/src/services/auth/sendRefreshToken.ts
@@ -5,7 +5,7 @@ export const sendRefreshToken = (res: Response, token: string) => {
     httpOnly: true,
     /** disable path for next.js server */
     // path: "/refresh_token",
-    domain: '.dorya-client.com',
+    domain: '.dorya-client.herokuapp.com',
     expires: new Date(Date.now() + 7 * 24 * 3600000)
   });
 };
