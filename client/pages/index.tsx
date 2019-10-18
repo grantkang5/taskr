66eb7484f4809da743e21f3e27bb41d64bb653bd
grantkang5 <g1k05j@gmail.com>
2019-10-17 18:17:48 -0700

    remove unused styles

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -5,12 +5,11 @@ import Layout from '../components/common/Layout';
  * Route: '/'
  * Api: Query currentUser's projects / Activity / Cards
  */
-import styles from './Dashboard.module.less'
 
 const Dashboard = () => {
   return (
     <Layout>
-      <div className={styles.main}>
+      <div>
         This is the dashboard
       </div>
     </Layout>
