ff1da50c726790f5fb4d5b2b183961e82675536b
grantkang5 <g1k05j@gmail.com>
2019-10-21 13:28:45 -0700

    Adds ioredis and redis config

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,27 @@
 import { ApolloServer, PubSub } from "apollo-server-express";
+import { RedisPubSub } from "graphql-redis-subscriptions";
 import { buildSchemaSync } from "type-graphql";
-import { UserResolver } from './resolvers/UserResolver'
 
-export const pubSub = new PubSub();
+import { UserResolver } from "./resolvers/UserResolver";
+import { PubSubRedisOptions } from "graphql-redis-subscriptions/dist/redis-pubsub";
+
+const redisConfig =
+  process.env.NODE_ENV === "production"
+    ? process.env.REDIS_URL
+    : {
+        host: process.env.REDIS_HOST,
+        port: process.env.REDIS_PORT
+      };
+
+export const pubSub =
+  process.env.NODE_ENV === "development"
+    ? new PubSub()
+    : new RedisPubSub(redisConfig as PubSubRedisOptions);
 
 export const server = new ApolloServer({
   schema: buildSchemaSync({
     resolvers: [UserResolver],
     pubSub
   }),
-  context: ({ req, res }) => ({ req, res }),
+  context: ({ req, res }) => ({ req, res })
 });
