4078b7d078c2749bc60acebbeefa8b5f5334bd03
grantkang5 <g1k05j@gmail.com>
2019-10-22 12:27:47 -0700

    Add redis for test and prod

--- server/src/services/redis.ts
+++ server/src/services/redis.ts
@@ -1,3 +1,12 @@
 import Redis from 'ioredis';
+import { RedisPubSub } from "graphql-redis-subscriptions";
 
-export const redis = new Redis(process.env.REDIS_PORT)
+const redisOptions = process.env.NODE_ENV === 'production'
+  ? process.env.REDIS_URL
+  : process.env.REDIS_PORT
+
+export const redis = new Redis(redisOptions)
+export const pubSub = new RedisPubSub({
+  publisher: new Redis(redisOptions),
+  subscriber: new Redis(redisOptions)
+})
