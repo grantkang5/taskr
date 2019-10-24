aef5341cfe9d4144aedb1810bff3b85bf477d6ce
jiminykbob <jikang92@hotmail.com>
2019-10-24 14:30:42 -0700

    add entities

---
+++ server/src/entity/Label.ts
@@ -0,0 +1,43 @@
+import {
+  BaseEntity,
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  ManyToMany,
+  JoinTable
+} from 'typeorm';
+import { ObjectType, Field, Int } from 'type-graphql';
+import { Project } from './Project';
+import { Task } from './Task';
+
+@ObjectType()
+@Entity('labels')
+export class Label extends BaseEntity {
+  @Field(() => Int)
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Field()
+  @Column()
+  name: string;
+
+  @Field()
+  @Column()
+  color: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  @ManyToOne(() => Project, project => project.labels)
+  project: Project;
+
+  @ManyToMany(() => Task)
+  @JoinTable()
+  tasks: Task[];
+}
