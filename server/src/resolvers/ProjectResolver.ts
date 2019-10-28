a8a105808b757e1bd2f7eeaf45b363f1db87425d
jiminykbob <jikang92@hotmail.com>
2019-10-28 16:21:42 -0700

    add update resolver

--- server/src/resolvers/ProjectResolver.ts
+++ server/src/resolvers/ProjectResolver.ts
@@ -1,4 +1,4 @@
-import { Mutation, Resolver, Arg, UseMiddleware, Ctx } from 'type-graphql';
+import { Mutation, Resolver, Arg, UseMiddleware, Ctx, Int } from 'type-graphql';
 import { isAuth } from '../services/auth/isAuth';
 import { Project } from '../entity/Project';
 import { MyContext } from '../services/context';
@@ -22,6 +22,34 @@ export class ProjectResolver {
       });
       // project.members = [user!];
       return await project.save();
+    } catch (err) {
+      console.log(err);
+      return err;
+    }
+  }
+
+  @Mutation(() => Boolean)
+  @UseMiddleware(isAuth)
+  async updateProject(
+    @Arg('id', () => Int) id: number,
+    @Arg('name', { nullable: true }) name?: string,
+    @Arg('desc', { nullable: true }) desc?: string
+  ) {
+    try {
+      const project = await Project.findOne({ where: { id } });
+      if (!project) {
+        throw new Error('Project does not exist');
+      }
+      if (name && project.name !== name) {
+        project.name = name;
+      }
+
+      if (desc && project.desc !== desc) {
+        project.desc = desc;
+      }
+
+      await project.save();
+      return true;
     } catch (err) {
       console.log(err);
       return err;
